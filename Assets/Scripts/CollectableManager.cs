74275ec95c49627e82751072f1542b759bc53638
JeremyBourg <jeremy.bourg@outlook.com>
2024-10-30 16:34:24 -0400

    Update CollectableManager.cs

--- Assets/Scripts/CollectableManager.cs
+++ Assets/Scripts/CollectableManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CollectableManager : MonoBehaviour
 {
     public int itemsCollected = 0;
+    [SerializeField]
+    private TMP_Text textCount;
 
     public void collectItem() 
     {
         itemsCollected++;
+        textCount.text = $"{itemsCollected}";
     }
 }
